3c015ef5a71a2ba9e3f6bf20b5f7f548b07f3fec
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-11 21:41:06 +0200

    Add remaining equation blocks

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ operator = gets.chomp
 if operator == '+'
     print "Enter number: "
     first_number = gets.chomp.to_i
+    if first_number
     print "Second number: "
     second_number = gets.chomp.to_i
 
@@ -15,6 +16,20 @@ elsif operator == '-'
     second_number = gets.chomp.to_i
 
     puts first_number - second_number
+elsif operator == '/'
+    print "Enter number: "
+    first_number = gets.chomp.to_i
+    print "Second number: "
+    second_number = gets.chomp.to_i
+
+    puts first_number / second_number
+elsif operator == '*'
+    print "Enter number: "
+    first_number = gets.chomp.to_i
+    print "Second number: "
+    second_number = gets.chomp.to_i
+
+    puts first_number * second_number
 else 
     puts "Invalid operator. Please try again."
 end
