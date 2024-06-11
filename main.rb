b0e312a124fa7f6cd3bafff47f0270339e468acd
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-11 21:36:26 +0200

    Add if block for addition operator

--- main.rb
+++ main.rb
@@ -1,2 +1,11 @@
 print "Welcome to the Calculator! Choose your operator (+, -, /, *): "
 operator = gets.chomp
+
+if operator == '+'
+    print "Enter number: "
+    first_number = gets.chomp.to_i
+    print "Second number: "
+    second_number = gets.chomp.to_i
+
+    puts first_number + second_number
+end
