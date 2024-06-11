9e85451a3f1a5546f49f75f017f3f4c0598c2279
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-11 21:38:28 +0200

    Add subtraction block

--- main.rb
+++ main.rb
@@ -8,6 +8,13 @@ if operator == '+'
     second_number = gets.chomp.to_i
 
     puts first_number + second_number
+elsif operator == '-'
+    print "Enter number: "
+    first_number = gets.chomp.to_i
+    print "Second number: "
+    second_number = gets.chomp.to_i
+
+    puts first_number - second_number
 else 
     puts "Invalid operator. Please try again."
 end
